2617cc9af1dfb1cc177c882f3308f8c2902d4717
Adam Starlight <adam.starlight@yandex.ru>
2021-08-12 09:34:43 +0300

    Migrated Wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = "ayu",
+  enable_tab_bar = false,
+  font = wezterm.font("Fira Code"),
+  font_size = 10.0,
+  window_padding = {
+    left = 8,
+    top = 8,
+    right = 8,
+    bottom = 8
+  }
+}

455b54ca8138856823bdc75ed40240b90cce190b
Adam Starlight <adam.starlight@yandex.ru>
2021-08-12 09:34:59 +0300

    Added links to Wezterm config

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ mkdir -p "$HOME/.config"
 PWD=$(dirname "$(readlink -f "$0")")
 
 ln -sfn "$PWD/nvim" "$HOME/.config/nvim"
+
 ln -sfn "$PWD/zsh/.zshrc" "$HOME/.zshrc"
 ln -sfn "$PWD/zsh/.zshrc.zni" "$HOME/.zshrc.zni"
 ln -sfn "$PWD/zsh/themes/mim.zsh-theme" "$HOME/.oh-my-zsh/themes/mim.zsh-theme"
+
+ln -sfn "$PWD/wezterm" "$HOME/.config/wezterm"
